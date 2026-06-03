c02b6de8ec0dfda92e6a06b784a45293232a2a29
ryanhugo070-source <ryanhugo070@gmail.com>
2026-06-03 13:06:03 -0300

    teste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import socket
 import datetime
+import time
 
 FIREBASE_URL = "https://automatizador-7d7d7-default-rtdb.firebaseio.com/"
 
@@ -16,11 +17,30 @@ def entrar_online():
     
     try:
         requests.put(url, json=dados, timeout=5)
-        print("[Firebase] Status 'Online' enviado!")
+        print(f"[Firebase] Voce esta ONLINE como: {nome_maquina}")
     except Exception as e:
-        print("[Firebase] Erro:", e)
+        print("[Firebase] Erro ao conectar:", e)
+
+def ficar_offline():
+    nome_maquina = socket.gethostname()
+    url = f"{FIREBASE_URL}usuarios_online/{nome_maquina}.json"
+    try:
+        requests.delete(url, timeout=5)
+        print("[Firebase] Voce saiu. Status 'Online' removido.")
+    except:
+        pass
 
 if __name__ == "__main__":
     entrar_online()
-    # Seu código de automação Python continuaria aqui...
-  
+    
+    print("\n>>> ROBO EM EXECUCAO <<<")
+    print("Mantenha esta janela aberta para continuar online.")
+    print("Pressione CTRL + C para fechar o robo.")
+    
+    try:
+        # Loop que mantem o script rodando e segurando o status online
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # Executa esta parte quando voce fecha o programa com CTRL+C
+        ficar_offline()
